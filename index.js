f0da88bbbc7318ecb694216e6911c897d39071b5
Som Bohora <som-bohora@ouhsc.edu>
2019-01-07 16:28:57 -0600

    update html

--- index.js
+++ index.js
@@ -1,33 +1,256 @@
+const STATES = [
+    {
+        name: "Alabama",
+        abbreviation: "AL"
+    },
+    {
+        name: "Alaska",
+        abbreviation: "AK"
+    },
+    {
+        name: "American Samoa",
+        abbreviation: "AS"
+    },
+    {
+        name: "Arizona",
+        abbreviation: "AZ"
+    },
+    {
+        name: "Arkansas",
+        abbreviation: "AR"
+    },
+    {
+        name: "California",
+        abbreviation: "CA"
+    },
+    {
+        name: "Colorado",
+        abbreviation: "CO"
+    },
+    {
+        name: "Connecticut",
+        abbreviation: "CT"
+    },
+    {
+        name: "Delaware",
+        abbreviation: "DE"
+    },
+    {
+        name: "District Of Columbia",
+        abbreviation: "DC"
+    },
+    {
+        name: "Federated States Of Micronesia",
+        abbreviation: "FM"
+    },
+    {
+        name: "Florida",
+        abbreviation: "FL"
+    },
+    {
+        name: "Georgia",
+        abbreviation: "GA"
+    },
+    {
+        name: "Guam",
+        abbreviation: "GU"
+    },
+    {
+        name: "Hawaii",
+        abbreviation: "HI"
+    },
+    {
+        name: "Idaho",
+        abbreviation: "ID"
+    },
+    {
+        name: "Illinois",
+        abbreviation: "IL"
+    },
+    {
+        name: "Indiana",
+        abbreviation: "IN"
+    },
+    {
+        name: "Iowa",
+        abbreviation: "IA"
+    },
+    {
+        name: "Kansas",
+        abbreviation: "KS"
+    },
+    {
+        name: "Kentucky",
+        abbreviation: "KY"
+    },
+    {
+        name: "Louisiana",
+        abbreviation: "LA"
+    },
+    {
+        name: "Maine",
+        abbreviation: "ME"
+    },
+    {
+        name: "Marshall Islands",
+        abbreviation: "MH"
+    },
+    {
+        name: "Maryland",
+        abbreviation: "MD"
+    },
+    {
+        name: "Massachusetts",
+        abbreviation: "MA"
+    },
+    {
+        name: "Michigan",
+        abbreviation: "MI"
+    },
+    {
+        name: "Minnesota",
+        abbreviation: "MN"
+    },
+    {
+        name: "Mississippi",
+        abbreviation: "MS"
+    },
+    {
+        name: "Missouri",
+        abbreviation: "MO"
+    },
+    {
+        name: "Montana",
+        abbreviation: "MT"
+    },
+    {
+        name: "Nebraska",
+        abbreviation: "NE"
+    },
+    {
+        name: "Nevada",
+        abbreviation: "NV"
+    },
+    {
+        name: "New Hampshire",
+        abbreviation: "NH"
+    },
+    {
+        name: "New Jersey",
+        abbreviation: "NJ"
+    },
+    {
+        name: "New Mexico",
+        abbreviation: "NM"
+    },
+    {
+        name: "New York",
+        abbreviation: "NY"
+    },
+    {
+        name: "North Carolina",
+        abbreviation: "NC"
+    },
+    {
+        name: "North Dakota",
+        abbreviation: "ND"
+    },
+    {
+        name: "Northern Mariana Islands",
+        abbreviation: "MP"
+    },
+    {
+        name: "Ohio",
+        abbreviation: "OH"
+    },
+    {
+        name: "Oklahoma",
+        abbreviation: "OK"
+    },
+    {
+        name: "Oregon",
+        abbreviation: "OR"
+    },
+    {
+        name: "Palau",
+        abbreviation: "PW"
+    },
+    {
+        name: "Pennsylvania",
+        abbreviation: "PA"
+    },
+    {
+        name: "Puerto Rico",
+        abbreviation: "PR"
+    },
+    {
+        name: "Rhode Island",
+        abbreviation: "RI"
+    },
+    {
+        name: "South Carolina",
+        abbreviation: "SC"
+    },
+    {
+        name: "South Dakota",
+        abbreviation: "SD"
+    },
+    {
+        name: "Tennessee",
+        abbreviation: "TN"
+    },
+    {
+        name: "Texas",
+        abbreviation: "TX"
+    },
+    {
+        name: "Utah",
+        abbreviation: "UT"
+    },
+    {
+        name: "Vermont",
+        abbreviation: "VT"
+    },
+    {
+        name: "Virgin Islands",
+        abbreviation: "VI"
+    },
+    {
+        name: "Virginia",
+        abbreviation: "VA"
+    },
+    {
+        name: "Washington",
+        abbreviation: "WA"
+    },
+    {
+        name: "West Virginia",
+        abbreviation: "WV"
+    },
+    {
+        name: "Wisconsin",
+        abbreviation: "WI"
+    },
+    {
+        name: "Wyoming",
+        abbreviation: "WY"
+    }
+]
+
 function generateOptionForm() {
     return `
     <main>
         <h1>Search National Parks by State</h1>
         <form id="js-park-search-form">
-            <label for="js-select-state">Choose a state:</label>
+            <label for="js-select-state">Select a state:</label>
             <select id="js-select-state">
-            ${STATES.map(states => `<option value=${states.abbreviation}>${states.name}</option>`)}
+            ${STATES.map(states => `<option value='${states.abbr}'>${states.name}</option>`)}
             </select>
-            <label for="limit">Enter a search limit</label>
+            <label for="limit">How many parks would you like to find?:</label>
             <input type="number" name="limit" id="limit" max="50">
             <input type="submit" value="Search">
         </form>
     </main>`
 };  
 
-// for (let i = 0; i < STATES.length; i++) {
-//     <option value=${STATES[i].name}>${STATES[i].abbreviation}</option>
-// }
-
-// function startPage() {
-// 	//Listen for button click to start quiz
-// 	$('#js-start-section').on('click', '.js-start-button', e => {
-// 		//show score and question number info
-// 		$('#js-question-info').show();
-// 		//Hide start section from view
-// 		$('#js-start-section').hide();
-// 		//increment current question;
-// 		handleCurrentQuestionNumber();
-// 		//load questions to question section
-// 		$('#js-questions').html($(generateOptionForm()));
-// 	});
-// }
+$('#js-search-results').html(generateOptionForm());
