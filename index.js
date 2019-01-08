9b0e2874050315da4882952d1d7c0397b3ef2187
Som Bohora <som-bohora@ouhsc.edu>
2019-01-08 13:49:24 -0600

    finish complex data request

--- index.js
+++ index.js
@@ -1,256 +1,59 @@
-const STATES = [
-    {
-        name: "Alabama",
-        abbreviation: "AL"
-    },
-    {
-        name: "Alaska",
-        abbreviation: "AK"
-    },
-    {
-        name: "American Samoa",
-        abbreviation: "AS"
-    },
-    {
-        name: "Arizona",
-        abbreviation: "AZ"
-    },
-    {
-        name: "Arkansas",
-        abbreviation: "AR"
-    },
-    {
-        name: "California",
-        abbreviation: "CA"
-    },
-    {
-        name: "Colorado",
-        abbreviation: "CO"
-    },
-    {
-        name: "Connecticut",
-        abbreviation: "CT"
-    },
-    {
-        name: "Delaware",
-        abbreviation: "DE"
-    },
-    {
-        name: "District Of Columbia",
-        abbreviation: "DC"
-    },
-    {
-        name: "Federated States Of Micronesia",
-        abbreviation: "FM"
-    },
-    {
-        name: "Florida",
-        abbreviation: "FL"
-    },
-    {
-        name: "Georgia",
-        abbreviation: "GA"
-    },
-    {
-        name: "Guam",
-        abbreviation: "GU"
-    },
-    {
-        name: "Hawaii",
-        abbreviation: "HI"
-    },
-    {
-        name: "Idaho",
-        abbreviation: "ID"
-    },
-    {
-        name: "Illinois",
-        abbreviation: "IL"
-    },
-    {
-        name: "Indiana",
-        abbreviation: "IN"
-    },
-    {
-        name: "Iowa",
-        abbreviation: "IA"
-    },
-    {
-        name: "Kansas",
-        abbreviation: "KS"
-    },
-    {
-        name: "Kentucky",
-        abbreviation: "KY"
-    },
-    {
-        name: "Louisiana",
-        abbreviation: "LA"
-    },
-    {
-        name: "Maine",
-        abbreviation: "ME"
-    },
-    {
-        name: "Marshall Islands",
-        abbreviation: "MH"
-    },
-    {
-        name: "Maryland",
-        abbreviation: "MD"
-    },
-    {
-        name: "Massachusetts",
-        abbreviation: "MA"
-    },
-    {
-        name: "Michigan",
-        abbreviation: "MI"
-    },
-    {
-        name: "Minnesota",
-        abbreviation: "MN"
-    },
-    {
-        name: "Mississippi",
-        abbreviation: "MS"
-    },
-    {
-        name: "Missouri",
-        abbreviation: "MO"
-    },
-    {
-        name: "Montana",
-        abbreviation: "MT"
-    },
-    {
-        name: "Nebraska",
-        abbreviation: "NE"
-    },
-    {
-        name: "Nevada",
-        abbreviation: "NV"
-    },
-    {
-        name: "New Hampshire",
-        abbreviation: "NH"
-    },
-    {
-        name: "New Jersey",
-        abbreviation: "NJ"
-    },
-    {
-        name: "New Mexico",
-        abbreviation: "NM"
-    },
-    {
-        name: "New York",
-        abbreviation: "NY"
-    },
-    {
-        name: "North Carolina",
-        abbreviation: "NC"
-    },
-    {
-        name: "North Dakota",
-        abbreviation: "ND"
-    },
-    {
-        name: "Northern Mariana Islands",
-        abbreviation: "MP"
-    },
-    {
-        name: "Ohio",
-        abbreviation: "OH"
-    },
-    {
-        name: "Oklahoma",
-        abbreviation: "OK"
-    },
-    {
-        name: "Oregon",
-        abbreviation: "OR"
-    },
-    {
-        name: "Palau",
-        abbreviation: "PW"
-    },
-    {
-        name: "Pennsylvania",
-        abbreviation: "PA"
-    },
-    {
-        name: "Puerto Rico",
-        abbreviation: "PR"
-    },
-    {
-        name: "Rhode Island",
-        abbreviation: "RI"
-    },
-    {
-        name: "South Carolina",
-        abbreviation: "SC"
-    },
-    {
-        name: "South Dakota",
-        abbreviation: "SD"
-    },
-    {
-        name: "Tennessee",
-        abbreviation: "TN"
-    },
-    {
-        name: "Texas",
-        abbreviation: "TX"
-    },
-    {
-        name: "Utah",
-        abbreviation: "UT"
-    },
-    {
-        name: "Vermont",
-        abbreviation: "VT"
-    },
-    {
-        name: "Virgin Islands",
-        abbreviation: "VI"
-    },
-    {
-        name: "Virginia",
-        abbreviation: "VA"
-    },
-    {
-        name: "Washington",
-        abbreviation: "WA"
-    },
-    {
-        name: "West Virginia",
-        abbreviation: "WV"
-    },
-    {
-        name: "Wisconsin",
-        abbreviation: "WI"
-    },
-    {
-        name: "Wyoming",
-        abbreviation: "WY"
+const apiKey = 'iaBarGgT3QLFcCGUavwLHM2ocQ398f8tojCVviYB'
+const searchURL = 'https://api.nps.gov/api/v1/parks'
+
+function formatQueryParams(params) {
+    const queryItems = Object.keys(params).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    return queryItems.join('&');
+}
+
+function createDiv(dataJson) {
+    if (dataJson.addresses[1]){
+        return `<div class="displayParks">
+        <h2>${dataJson.fullName}</h2>
+        <p>${dataJson.description}</p>
+        <address>
+            ${dataJson.addresses[1].line1}<br>
+            ${dataJson.addresses[1].line2}
+            ${dataJson.addresses[1].line3}<br>
+            ${dataJson.addresses[1].city}
+            ${dataJson.addresses[1].stateCode}
+            ${dataJson.addresses[1].postalCode}
+        </address>
+        <a href="${dataJson.url}" target="_blank">More Info</a>
+    </div>`
     }
-]
+    return ''
 
-function generateOptionForm() {
-    return `
-    <main>
-        <h1>Search National Parks by State</h1>
-        <form id="js-park-search-form">
-            <label for="js-select-state">Select a state:</label>
-            <select id="js-select-state">
-            ${STATES.map(states => `<option value='${states.abbr}'>${states.name}</option>`)}
-            </select>
-            <label for="limit">How many parks would you like to find?:</label>
-            <input type="number" name="limit" id="limit" max="50">
-            <input type="submit" value="Search">
-        </form>
-    </main>`
-};  
+}
 
-$('#js-search-results').html(generateOptionForm());
+function loadData(dataJson) {
+    $('#search-results').html(dataJson.data.map(createDiv));
+}
+
+function getParksData(state, limit){
+    const params = {
+        limit,
+        stateCode: state,
+        api_key: apiKey,
+        fields: 'addresses'
+    }
+
+    const queryString = formatQueryParams(params);
+    const url = searchURL + '?' + queryString
+    fetch(url)
+        .then(response => response.json()
+        .then(responseJson => loadData(responseJson))
+        .catch(error => console.log(error)))
+}
+
+function watchForm() {
+    $('#search-form').submit(event => {
+        event.preventDefault();
+        const stateValue = $('#state-select').val();
+        const limit = ($('#search-limit').val() || 5)-1;
+        $('#search-results-h2').html("<h2>Here are the results!</h2>")
+        getParksData(stateValue, limit);
+    })
+}
+
+$(watchForm)
